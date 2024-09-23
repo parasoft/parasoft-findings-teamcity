2d8c2b55ec715b487dc665c3a7f8be6daa344490
Roller Wang <rwang@parasoft.com>
2024-09-23 10:41:02 +0800

    Not use ArrayList to avoid OOM

--- parasoft-findings-teamcity-agent/src/main/java/com/parasoft/findings/teamcity/agent/PmdReportParseHandler.java
+++ parasoft-findings-teamcity-agent/src/main/java/com/parasoft/findings/teamcity/agent/PmdReportParseHandler.java
@@ -1,34 +1,42 @@
 package com.parasoft.findings.teamcity.agent;
 
+import jetbrains.buildServer.agent.AgentRunningBuild;
+import jetbrains.buildServer.messages.DefaultMessagesInfo;
+import org.springframework.util.StringUtils;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PmdReportParseHandler extends DefaultHandler {
-    private String currentFileName;
-    private StringBuilder currentTextOfViolationNode = new StringBuilder();
-    private PmdViolation currentPmdViolation;
 
-    private final List<PmdViolation> pmdViolations;
+    private final AgentRunningBuild _build;
+    private final RuleDocumentationUrlProvider _ruleDocumentationUrlProvider;
+    private final Set<String> _inspectionTypeIds;
 
-    public PmdReportParseHandler() {
-        pmdViolations = new ArrayList<>();
+    private String _currentFileName;
+    private final StringBuilder _currentTextOfViolationNode;
+    private PmdViolation _currentPmdViolation;
+
+    public PmdReportParseHandler(AgentRunningBuild build, RuleDocumentationUrlProvider _ruleDocumentationUrlProvider) {
+        this._build = build;
+        this._ruleDocumentationUrlProvider = _ruleDocumentationUrlProvider;
+        this._currentTextOfViolationNode = new StringBuilder();
+        this._inspectionTypeIds = new HashSet<>();
     }
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
-        currentTextOfViolationNode.setLength(0);
+        _currentTextOfViolationNode.setLength(0);
 
         if ("file".equals(qName)) {
-            currentFileName = attributes.getValue("name");
+            _currentFileName = attributes.getValue("name");
         }
 
         if ("violation".equals(qName)) {
-            currentPmdViolation = new PmdViolation()
-                .setFileName(currentFileName)
+            _currentPmdViolation = new PmdViolation()
+                .setFileName(_currentFileName)
                 .setRule(attributes.getValue("rule"))
                 .setruleSet(attributes.getValue("ruleset"))
                 .setruleAnalyzer(attributes.getValue("ruleanalyzer"))
@@ -43,25 +51,85 @@ public class PmdReportParseHandler extends DefaultHandler {
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
-        currentTextOfViolationNode.append(ch, start, length);
+        _currentTextOfViolationNode.append(ch, start, length);
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         if (qName.equals("violation")) {
-            currentPmdViolation = currentPmdViolation.setMessage(currentTextOfViolationNode.toString().trim());
-            pmdViolations.add(currentPmdViolation);
-            currentPmdViolation = null;
+            _currentPmdViolation = _currentPmdViolation.setMessage(_currentTextOfViolationNode.toString().trim());
+            uploadInspectionAndType(_currentPmdViolation);
+            _currentPmdViolation = null;
         } else if (qName.equals("file")) {
-            currentFileName = null;
+            _currentFileName = null;
         }
     }
 
-    public List<PmdViolation> getPmdViolations() {
-        return pmdViolations;
+    private void uploadInspectionAndType(PmdViolation pmdViolation) {
+        String cit_rule = pmdViolation.getRule();
+        String cit_category = pmdViolation.getruleSet();
+        String ruleAnalyzerId = pmdViolation.getruleAnalyzer();
+
+        String cit_descriptionOrUrl = null;
+        if (_ruleDocumentationUrlProvider != null) {
+            if (StringUtils.isEmpty(ruleAnalyzerId)) {
+                String violationType = pmdViolation.getType();
+                String categoryId = pmdViolation.getcategoryId();
+                ruleAnalyzerId = mapToAnalyzer(violationType, categoryId);
+            }
+            cit_descriptionOrUrl = _ruleDocumentationUrlProvider.getRuleDocUrl(ruleAnalyzerId, cit_rule);
+        }
+        if (cit_descriptionOrUrl == null) {
+            cit_descriptionOrUrl = "<html><body>"+escapeString(pmdViolation.getruleDescription())+"</body></html>";
+        }
+        String ci_message = pmdViolation.getMessage();
+        String ci_line = pmdViolation.getbeginLine();
+        String ci_fileLocation = pmdViolation.getFileName();
+        String ci_severityNumber = pmdViolation.getPriority();
+
+        if (!_inspectionTypeIds.contains(cit_rule)) {
+            _inspectionTypeIds.add(cit_rule);
+            _build.getBuildLogger().logMessage(DefaultMessagesInfo.createTextMessage
+                    ("##teamcity[inspectionType id='"+cit_rule+"' name='"+cit_rule+"' description='"+cit_descriptionOrUrl+"' category='"+escapeString(cit_category)+"']"));
+        }
+        _build.getBuildLogger().logMessage(DefaultMessagesInfo.createTextMessage
+                ("##teamcity[inspection typeId='"+cit_rule+"' message='"+escapeString(ci_message)+"' file='"+ci_fileLocation+"' line='"+ci_line+"' SEVERITY='"+convertSeverity(ci_severityNumber)+"']"));
     }
 
-    public class PmdViolation {
+    private String mapToAnalyzer(String violationType, String categoryId) {
+        switch (violationType){
+            case "DupViol":
+                return "com.parasoft.xtest.cpp.analyzer.static.dupcode";
+            case "FlowViol":
+                return "com.parasoft.xtest.cpp.analyzer.static.flow";
+            case "MetViol":
+                return "com.parasoft.xtest.cpp.analyzer.static.metrics";
+            default:
+                if ("GLOBAL".equals(categoryId)) {
+                    return "com.parasoft.xtest.cpp.analyzer.static.global";
+                }
+                return "com.parasoft.xtest.cpp.analyzer.static.pattern";
+        }
+    }
+
+    private String convertSeverity(String severityNumber) {
+        if (Objects.equals(severityNumber, "1")) {
+            return "ERROR";
+        } else {
+            return "WARNING";
+        }
+    }
+
+    private String escapeString(String str) {
+        return str.replace("|", "||")
+                .replace("'", "|'")
+                .replace("[", "|[")
+                .replace("]", "|]")
+                .replace("\n", "|n")
+                .replace("\r", "|r");
+    }
+
+    private static class PmdViolation {
         private String fileName;
         private String rule;
         private String ruleSet;
